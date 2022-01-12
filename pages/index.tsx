c6abd8ef4da270f45fe4a8fc92c93e5caf6f11eb
Eduardo Aguiar <edu.pessoal@outlook.com>
2022-01-12 16:59:35 -0300

    remove erros

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,6 @@ const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         {/* Title */}
         <title>Livero | Entregas e coletas</title>
