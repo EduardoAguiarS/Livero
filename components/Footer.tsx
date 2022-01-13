92cf04423bbc9e08359d4c7adc508086b76f5b16
Eduardo Aguiar <edu.pessoal@outlook.com>
2022-01-13 16:06:20 -0300

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,8 +2,16 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className="px-10 py-4 text-center text-white bg-red">
+      <p>
+        ©2022 Livero - Quase todos direitos reservados. |{" "}
+        <a
+          href="https://www.linkedin.com/in/eduardo-aguiar-s/"
+          target={`_blank`}
+        >
+          Eduardo Aguiar
+        </a>
+      </p>
     </div>
   );
 }
